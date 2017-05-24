5201bf579fe6f388dbc4c12b8c80f9a05cf09aea
Linielson Rosa <linielson@asseinfo.com.br>
2017-05-23 21:56:56 -0300

    Clear the code to avoid encodings. Good Code.

---
+++ src/significant_names/avoid_encodings.js
@@ -0,0 +1,6 @@
+// We have enough encodings to deal with without adding more to our burden. Encoding
+// type or scope information into names simply adds an extra burden of deciphering. It
+// hardly seems reasonable to require each new employee to learn yet another encoding “language”
+// in addition to learning the (usually considerable) body of code that they’ll be working
+// in. It is an unnecessary mental burden when trying to solve a problem. Encoded names
+// are seldom pronounceable and are easy to mis-type.
