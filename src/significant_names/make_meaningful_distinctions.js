324f43f6b70fe1b4d3a460d5d1c1ea7c80f45093
Linielson Rosa <linielson@asseinfo.com.br>
2017-05-23 21:24:28 -0300

    Clear the code to make meaningful distinctions. Good Code.

---
+++ src/significant_names/make_meaningful_distinctions.js
@@ -0,0 +1,7 @@
+function copyChars(origin, destination) {
+  for (var i = 0; i < origin.length; i++) {
+    destination[i] = origin[i];
+  }
+}
+
+function getActiveAccount() { }
--- src/significant_names/significant_distinctions.js
+++
@@ -1,9 +0,0 @@
-function copyChars(ai, a2) {
-  for (var i = 0; i < a1.length; i++) {
-    a2[i] = a1[i];
-  }
-}
-
-function getActiveAccount() { }
-function getActiveAccounts() { }
-function getActiveAccountInfo() { }
