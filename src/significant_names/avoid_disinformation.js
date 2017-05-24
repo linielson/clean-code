d5bcade6d0a4ede6862511e7f27d31d32472eb38
Linielson Rosa <linielson@asseinfo.com.br>
2017-05-23 21:11:41 -0300

    Clear the code to avoid disinformation. Good Code.

---
+++ src/significant_names/avoid_disinformation.js
@@ -0,0 +1,21 @@
+var hypotenuse;
+var accountList = [];
+
+var accountGroup;
+var accounts;
+var bunchOfAccounts;
+
+function handlingStrings() { }
+
+function storageStrings() { }
+
+var last_selected = 0;
+var option_selected = 1;
+var first_option = 2;
+const new_option = 10;
+
+if (option_selected == last_selected) {
+  last_selected = first_option;
+} else {
+  last_selected = new_option;
+}
