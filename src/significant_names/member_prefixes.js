c41ad45605412f021fdc44252941f1bb95b4f6e4
Linielson Rosa <linielson@asseinfo.com.br>
2017-05-26 20:29:49 -0300

    Member Prefixes. Good Code.

--- src/significant_names/member_prefixes.js
+++ src/significant_names/member_prefixes.js
@@ -1,6 +1,6 @@
 class Part {
-  var m_dsc; // The textual description
-  function setName(name) {
-    m_dsc = name;
+  var description;
+  function setdescription(description) {
+    this.description = description;
   }
 }
