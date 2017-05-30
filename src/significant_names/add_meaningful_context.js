a891eea4e23948e8d44a3c3a2c53e51f7c128571
Linielson Rosa <linielson@asseinfo.com.br>
2017-05-30 20:19:36 -0300

    Clear the code to add meaningful context. Good Code.

--- src/significant_names/add_meaningful_context.js
+++ src/significant_names/add_meaningful_context.js
@@ -1,22 +1,46 @@
-var PrintGuessStatistics = function(candidate, count) {
+var GuessStatisticsMessage = function() {
   var number;
   var verb;
   var pluralModifier;
 
-  if (count == 0) {
+  var _make = function(candidate, count) {
+    createPluralDependentMessageParts(count);
+    return format(candidate);
+  };
+
+  function createPluralDependentMessageParts(count) {
+    if (count == 0) {
+      thereAreNoLetters();
+    } else if (count == 1) {
+      thereIsOneLetter();
+    } else {
+      thereAreManyLetters(count);
+    }
+  }
+
+  function thereAreNoLetters() {
     number = "no";
-    verb = "are";
-    pluralModifier = "s";
-  } else if (count == 1) {
-    number = "1";
-    verb = "is";
-    pluralModifier = "";
-  } else {
-    number = parseInt(count);
     verb = "are";
     pluralModifier = "s";
   }
 
-  var guessMessage = format("There %s %s %s%s", verb, number, candidate, pluralModifier);
-  print(guessMessage);
+  function thereIsOneLetter() {
+    number = "1";
+    verb = "is";
+    pluralModifier = "";
+  }
+
+  function thereAreManyLetters(count) {
+    number = count.toString();
+    verb = "are";
+    pluralModifier = "s";
+  }
+
+  function format(candidate) {
+    return `There ${verb} ${number} ${candidate}${pluralModifier}`;
+  }
+
+  return {
+    make: _make
+  }
 };
