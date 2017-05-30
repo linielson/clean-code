44489d3dc4d181c350e4e062b4d2e00704c31baf
Linielson Rosa <linielson@asseinfo.com.br>
2017-05-30 20:18:36 -0300

    Examples with domain names.

---
+++ src/significant_names/use_problem_domain_names.js
@@ -0,0 +1,8 @@
+/*
+  When there is no “programmer-eese” for what you’re doing, use the name from the problem
+domain. At least the programmer who maintains your code can ask a domain expert
+what it means.
+  Separating solution and problem domain concepts is part of the job of a good programmer
+and designer. The code that has more to do with problem domain concepts
+should have names drawn from the problem domain.
+*/
---
+++ src/significant_names/use_solution_domain_names.js
@@ -0,0 +1,11 @@
+/*
+  Remember that the people who read your code will be programmers. So go ahead and use
+computer science (CS) terms, algorithm names, pattern names, math terms, and so forth.
+It is not wise to draw every name from the problem domain because we don’t want our
+coworkers to have to run back and forth to the customer asking what every name means
+when they already know the concept by a different name.
+  The name AccountVisitor means a great deal to a programmer who is familiar with
+the VISITOR pattern. What programmer would not know what a JobQueue was? There are
+lots of very technical things that programmers have to do. Choosing technical names for
+those things is usually the most appropriate course.
+*/
